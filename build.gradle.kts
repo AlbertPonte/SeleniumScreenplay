d73729f7002375dd18207ed2c2d6ceea1a9138a4
AlbertPonte <prcmalbert@gmail.com>
2024-09-25 15:34:39 -0500

    comemtando cucumber6

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     implementation ("net.serenity-bdd:serenity-core:4.2.1")
     implementation ("net.serenity-bdd:serenity-junit5:4.2.1")
     implementation ("net.serenity-bdd:serenity-cucumber:4.2.1")
-    implementation("net.serenity-bdd:serenity-cucumber6:2.6.0")
+    //implementation("net.serenity-bdd:serenity-cucumber6:2.6.0")
     implementation ("net.serenity-bdd:serenity-ensure:4.2.1")
     implementation ("net.serenity-bdd:serenity-screenplay:4.2.1")
     implementation ("net.serenity-bdd:serenity-screenplay-webdriver:4.2.1")
